70abe753556875e949fcf1ee9904245b08cc1515
notautogenerated2365 <personq12345@gmail.com>
2025-10-16 19:24:34 -0400

    rename .ipp to .tpp

--- make/make.cpp
+++ make/make.cpp
@@ -265,7 +265,7 @@ namespace ezsimd {
     header 
         << "\n} // namespace ezsimd"
         << "\n"
-        << "\n#include \"libezsimd.ipp\" // full definitions for templates declared in this file only"
+        << "\n#include \"libezsimd.tpp\" // full definitions for templates declared in this file only"
         << "\n// templates declared in ezsimd.hpp are simply defined in ezsimd.hpp"
     ;
     templ << "\n} // namespace ezsimd";
@@ -275,7 +275,7 @@ int main() {
     ofstream source("../ezsimd.hpp");
     ofstream header("../libezsimd.hpp");
     ofstream library("../ezsimd.cpp");
-    ofstream templ("../libezsimd.ipp");
+    ofstream templ("../libezsimd.tpp");
 
     if (!source.is_open()) {
         cerr << "Error opening output source file\n";
